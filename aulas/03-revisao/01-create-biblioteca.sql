d813de38a5140118d13d16301df0bbc573ec8aa5
danpochini <pocchini@live.dk>
2017-01-30 18:42:21 -0200

    Exercícios de revisão das aulas anteriores

---
+++ aulas/03-revisao/01-create-biblioteca.sql
@@ -0,0 +1,49 @@
+create database Biblioteca
+
+go
+
+use Biblioteca
+
+go
+
+/*  
+1. Criar as tabelas relacionadas anteriormente, utilizando uma instrução SQL sem especificar as (chaves) restrições de integridade.
+ 
+ 
+*/ 
+
+create table Assunto(
+	sg_Assunto char(1) not null,
+	ds_Assunto varchar(30)
+)
+go
+
+create table Editora(
+	cd_Editora int not null,
+	nm_Editora varchar(30)
+)
+go
+
+create table Autor(
+	cd_Autor int not null,
+	nm_Autor varchar(40),
+	ds_EnderecoAutor varchar(60),
+	dt_Nascimento date
+)
+go
+
+create table Livro(
+	cd_Livro int not null, 
+	nm_Titulo varchar(30),
+	vl_Livro money,
+	dt_Lancamento date,
+	cd_Editora int not null,
+	sg_Assunto char(1) 
+)
+go
+
+create table Autor_Livro(
+	cd_Livro int not null,
+	cd_Autor int not null
+)
+go
---
+++ aulas/03-revisao/02-alter-biblioteca.sql
@@ -0,0 +1,34 @@
+use Biblioteca
+
+go
+
+/*  
+Altere as tabelas de forma a incluir as restrições de integridade
+*/  
+
+
+alter table Assunto
+	add primary key (sg_Assunto)
+
+
+alter table Editora
+	add primary key (cd_Editora)
+go
+
+alter table Autor
+	add primary key (cd_Autor)
+go
+
+alter table Livro
+	add primary key (cd_Livro)
+go
+
+alter table Livro
+	add foreign key (cd_Editora)
+	references Editora
+go
+
+alter table Livro 
+	add foreign key (sg_Assunto)
+	references Assunto
+go
